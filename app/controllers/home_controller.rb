fcf32b6ef97e748518b5afff36b0b8f2f1a23952
Fedor Lukyanov <lukyanov.f.ua@gmail.com>
2017-08-22 15:25:22 +0300

    Add puma config for production

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ActionController::Base
+  def index
+    head 200
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
   resources :users, only: [:create]
   resources :sessions, only: [:create]
 end
