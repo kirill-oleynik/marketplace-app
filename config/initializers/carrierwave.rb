1f6154cad95d82e2800b15f180a69730a5c3ecd3
Fedor Lukyanov <lukyanov.f.ua@gmail.com>
2017-09-15 13:21:24 +0300

    Change carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,14 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
+  config.enable_processing = false
+
   if Rails.env.production?
+    config.fog_provider = 'fog/aws'
+    config.fog_public = false
+    config.fog_directory = ENV['AWS_BUCKET']
     config.fog_credentials = {
       provider: 'AWS',
       region: ENV['AWS_REGION'],
@@ -7,12 +16,9 @@ CarrierWave.configure do |config|
       aws_secret_access_key: ENV['AWS_SECRET_KEY']
     }
     config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_public = false
-    config.fog_directory = ENV['AWS_BUCKET']
   else
-    config.storage = :file
     config.root = File.join(Rails.root, 'public')
     config.asset_host = ENV['ASSET_HOST']
+    config.storage = :file
   end
 end
