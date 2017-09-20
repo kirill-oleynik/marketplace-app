431df5bf37cd16a44c55e745695e3157abec0034
kirill-oleynik <kirill.olejnik@gmail.com>
2017-09-20 10:14:21 +0300

    Add current user's review to full-app serializer'

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :application
+
+  def self.find_value_by_user_and_application(application:, user:)
+    find_by(application: application, user: user).try(:value)
+  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,4 +3,25 @@ require 'rails_helper'
 RSpec.describe Review do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:application) }
+
+  describe '#find_value_by_user_and_application', :with_db_cleaner do
+    let(:review_value) do
+      described_class.find_value_by_user_and_application(
+        user: user,
+        application: application
+      )
+    end
+
+    let(:user) { create(:user) }
+    let(:application) { create(:application) }
+    let(:review_value) { 1 }
+
+    before do
+      create(:review, value: review_value, user: user, application: application)
+    end
+
+    it 'returns review value created by given user' do
+      expect(review_value).to eq(review_value)
+    end
+  end
 end
