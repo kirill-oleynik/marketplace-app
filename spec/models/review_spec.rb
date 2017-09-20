82c0a5e0a27f733eeec3b02a826ab07b8035e23b
kirill-oleynik <kirill.olejnik@gmail.com>
2017-09-20 14:06:06 +0300

    Fix review model spec

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -14,14 +14,14 @@ RSpec.describe Review do
 
     let(:user) { create(:user) }
     let(:application) { create(:application) }
-    let(:review_value) { 1 }
+    let(:value) { 1 }
 
     before do
-      create(:review, value: review_value, user: user, application: application)
+      create(:review, value: value, user: user, application: application)
     end
 
     it 'returns review value created by given user' do
-      expect(review_value).to eq(review_value)
+      expect(review_value).to eq(value)
     end
   end
 end
